58109e85ae05e12d1404b14fff8fc812128bde4b
Michael Panik <michaelpanik@Michaels-MacBook-Pro-2.local>
2018-09-10 11:36:41 -0500

    Aria label navigation button. Add text to social links.

--- themes/michaelpanik/source/js/main.js
+++ themes/michaelpanik/source/js/main.js
@@ -4,16 +4,24 @@ const body = document.body
 menuToggle.addEventListener('click', function() {
     if (body.classList.contains('mobile-nav_active')) {
         body.classList.remove('mobile-nav_active')
+        this.setAttribute('aria-expanded', 'true')
     } else {
         body.classList.add('mobile-nav_active')
+        this.setAttribute('aria-expanded', 'false')
     }
 })
 
 const tooltipList = document.querySelectorAll('.tooltip')
 
 tooltipList.forEach(function(el) {
-    const tooltip = document.createElement('label')
+    const tooltip = document.createElement('span')
     tooltip.classList.add('tooltipBubble')
+    
+    var att = document.createAttribute("aria-role")
+    att.value = "tooltip"
+    tooltip.setAttributeNode(att)
+    
     tooltip.innerHTML = el.dataset.tooltip
+    
     el.appendChild(tooltip)
 })
